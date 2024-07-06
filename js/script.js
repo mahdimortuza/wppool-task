28fdb3ddf84a74928851ecb32ed63e91f7e5d9fb
mahdimortuza <mahdi.webx@gmail.com>
2024-07-06 13:01:45 +0600

    table added

--- js/script.js
+++ js/script.js
@@ -32,43 +32,127 @@ logoBlack.src = "./images/logos/logo-black.png"
 const mainLogo = document.getElementById("main-logo")
 
 
-window.onscroll = () => {
-    if (window.scrollY > 600) {
-        mainHeader.classList.add('sticky-header');
-        mainHeader.classList.remove("fixed-header")
+// window.onscroll = () => {
+//     if (window.scrollY > 600) {
+//         mainHeader.classList.add('sticky-header');
+//         mainHeader.classList.remove("fixed-header")
 
-        mainNavbar.classList.add("py-[0px]");
-        mainNavbar.classList.remove("py-[34px]")
+//         mainNavbar.classList.add("py-[0px]");
+//         mainNavbar.classList.remove("py-[34px]")
 
-        shareButton.classList.add("share-btn-green");
-        shareButton.classList.remove("share-btn-white")
-        downloadButton.classList.add('download-btn-blue');
-        downloadButton.classList.remove("download-btn-white")
+//         shareButton.classList.add("share-btn-green");
+//         shareButton.classList.remove("share-btn-white")
+//         downloadButton.classList.add('download-btn-blue');
+//         downloadButton.classList.remove("download-btn-white")
         
-        if (mainLogo.contains(logoWhite)) {
-            mainLogo.removeChild(logoWhite)
-        }
-        if (!mainLogo.contains(logoBlack)) {
-            mainLogo.appendChild(logoBlack)
-        }
-    } else {
-        mainHeader.classList.add("fixed-header");
-        mainHeader.classList.remove("sticky-header")
+//         if (mainLogo.contains(logoWhite)) {
+//             mainLogo.removeChild(logoWhite)
+//         }
+//         if (!mainLogo.contains(logoBlack)) {
+//             mainLogo.appendChild(logoBlack)
+//         }
+//     } else {
+//         mainHeader.classList.add("fixed-header");
+//         mainHeader.classList.remove("sticky-header")
         
-        mainNavbar.classList.add("py-[34px]");
-        mainNavbar.classList.remove("py-[0px]")
+//         mainNavbar.classList.add("py-[34px]");
+//         mainNavbar.classList.remove("py-[0px]")
 
-        shareButton.classList.add('share-btn-white');
-        shareButton.classList.remove("share-btn-green")
-        downloadButton.classList.add('download-btn-white');
-        downloadButton.classList.remove("download-btn-blue")
+//         shareButton.classList.add('share-btn-white');
+//         shareButton.classList.remove("share-btn-green")
+//         downloadButton.classList.add('download-btn-white');
+//         downloadButton.classList.remove("download-btn-blue")
         
-        if (mainLogo.contains(logoBlack)) {
-            mainLogo.removeChild(logoBlack)
-        }
-        if (!mainLogo.contains(logoWhite)) {
-            mainLogo.appendChild(logoWhite)
-        }
+//         if (mainLogo.contains(logoBlack)) {
+//             mainLogo.removeChild(logoBlack)
+//         }
+//         if (!mainLogo.contains(logoWhite)) {
+//             mainLogo.appendChild(logoWhite)
+//         }
 
+//     }
+// };
+
+// chart logics starts here
+
+const options = {
+    chart: {
+        id: 'chart',
+        type: 'line',
+        height: 400,
+        toolbar: {
+            autoSelected: 'pan',
+            show: false
+        }
+    },
+    colors: ['#FC714D', '#615DE3', '#AFCD80', '#6F34A1',  ],
+    stroke: {
+        width: [2, 2, 2, 2]
+    },
+    dataLabels: {
+        enabled: false
+    },
+    fill: {
+        opacity: 1,
+    },
+    markers: {
+        size: 0
+    },
+    series: [
+        {
+            name: 'WPPOOL',
+            data: generateDayWiseTimeSeries(new Date('Feb 2024').getTime(), 185, {
+                min: 0   ,
+                max: 100
+            })
+        },
+        {
+            name: 'Google',
+            data: generateDayWiseTimeSeries(new Date('Feb 2024').getTime(), 185, {
+                min: 0,
+                max: 100
+            })
+        },
+        {
+            name: 'Microsoft',
+            data: generateDayWiseTimeSeries(new Date('Feb 2024').getTime(), 185, {
+                min: 0,
+                max: 100
+            })
+        },
+        {
+            name: 'Twitter',
+            data: generateDayWiseTimeSeries(new Date('Feb 2024').getTime(), 185, {
+                min: 0,
+                max: 100
+            })
+        }
+    ],
+    xaxis: {
+        type: 'datetime'
     }
-};
+}
+
+var chart = new ApexCharts(
+    document.querySelector("#chart"),
+    options
+);
+
+chart.render();
+
+function generateDayWiseTimeSeries(baseval, count, yrange) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+        var x = baseval;
+        var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+        series.push([x, y]);
+        baseval += 86400000;
+        i++;
+    }
+    return series;
+}
+
+
+  
